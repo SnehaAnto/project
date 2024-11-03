86b808cfbe250d62f5c0e3d234d7690e05fa3e85
Sneha M Antonious <snea913@gmail.com>
2024-11-03 15:31:14 -0500

    Reorganized folders

--- assignment2/server/src/app.module.ts
+++ assignment2/server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TimesheetModule } from './timesheet/timesheet.module';
 import { ConfigModule } from '@nestjs/config';
+import { LoginModule } from './login/login.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot(), 
         MongooseModule.forRoot(process.env.MONGODB_URI, {}),
-        TimesheetModule
+        TimesheetModule,
+        LoginModule
     ],
 })
 export class AppModule {}
