be1b391f569a0b4163e22af77bb496ac0a188225
Sneha M Antonious <snea913@gmail.com>
2024-12-04 23:26:31 -0500

    Title updates

--- assignment4/webapp/src/app/login/page.tsx
+++ assignment4/webapp/src/app/login/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next';
 import Login from '../ui/login/login';
 
 export default function LoginPage() {
   return <Login />;
-} 
+}
+
+export const metadata: Metadata = {
+  title: 'Login',
+}; 
--- assignment4/webapp/src/app/timesheet/page.tsx
+++ assignment4/webapp/src/app/timesheet/page.tsx
@@ -1,5 +1,9 @@
 import Timesheet from '../ui/timesheet/timesheet';
+import { Metadata } from 'next';
 
 export default function TimesheetPage() {
   return <Timesheet />;
 } 
+export const metadata: Metadata = {
+  title: 'Timesheet',
+}; 
